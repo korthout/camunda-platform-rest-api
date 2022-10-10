dab1c0794e4d25318978c3af4c10fb8507f4aeaa
Nico Korthout <nico.korthout@camunda.com>
2022-10-10 22:54:37 +0200

    feat: support GET /status
    Implements the GET /status by sending a topology request to a zeebe
    cluster.
    
    Since the Zeebe client is injected, it could be that it isn't running
    yet when the /status is requested.
    
    Other failure cases related to the client request itself, are not yet
    dealt with.

--- src/main/kotlin/com/github/korthout/zeeberestclient/StatusController.kt
+++ src/main/kotlin/com/github/korthout/zeeberestclient/StatusController.kt
@@ -1,5 +1,10 @@
 package com.github.korthout.zeeberestclient
 
+import io.camunda.zeebe.client.api.response.Topology
+import io.camunda.zeebe.spring.client.lifecycle.ZeebeClientLifecycle
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/status")
 class StatusController {
 
-  @GetMapping fun getStatus(): String = "Not yet implemented"
+  @Autowired lateinit var client: ZeebeClientLifecycle
+
+  /** Retrieves the Topology of a Zeebe cluster. */
+  @GetMapping
+  fun getStatus(): ResponseEntity<Topology> =
+    if (!client.isRunning) ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+    else {
+      val topology = client.newTopologyRequest().send().join()
+      // todo: handle exceptional cases
+      ResponseEntity.ok(topology)
+    }
 }
