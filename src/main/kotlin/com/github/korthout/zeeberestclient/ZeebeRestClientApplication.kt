3719ca42847afc319fdee46191f54c1d06b6aa50
Nico Korthout <nico.korthout@camunda.com>
2022-10-10 22:02:01 +0200

    style: apply ktfmt formatting

--- src/main/kotlin/com/github/korthout/zeeberestclient/ZeebeRestClientApplication.kt
+++ src/main/kotlin/com/github/korthout/zeeberestclient/ZeebeRestClientApplication.kt
@@ -3,9 +3,8 @@ package com.github.korthout.zeeberestclient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class ZeebeRestClientApplication
+@SpringBootApplication class ZeebeRestClientApplication
 
 fun main(args: Array<String>) {
-	runApplication<ZeebeRestClientApplication>(*args)
+  runApplication<ZeebeRestClientApplication>(*args)
 }
