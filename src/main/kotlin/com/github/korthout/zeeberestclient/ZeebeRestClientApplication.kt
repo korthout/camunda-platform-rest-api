15f5e3e97c9bd9c254b181ad5616121040c8e393
Nico Korthout <nico.korthout@camunda.com>
2022-10-10 21:34:46 +0200

    build: add std spring-boot prj

---
+++ src/main/kotlin/com/github/korthout/zeeberestclient/ZeebeRestClientApplication.kt
@@ -0,0 +1,11 @@
+package com.github.korthout.zeeberestclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZeebeRestClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZeebeRestClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/korthout/zeeberestclient/ZeebeRestClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.korthout.zeeberestclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZeebeRestClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
