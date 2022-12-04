1f3d080742416354046ebe2777b71232a081a99a
Nico Korthout <nico.korthout@camunda.com>
2022-12-04 22:11:56 +0100

    test: get status should respond with topology
    This commit adds a first test to verify the behavior of the controllers.
    Sepcifically, on GET /status we expect that it responds with the
    topology provided by the Zeebe Client.
    
    A FakeZeebeClient is provided to act in behalf of the real ZeebeClient
    in tests. This required a small amount of glue-code, but in the end
    provides simple usage in tests.

---
+++ src/test/kotlin/com/github/korthout/zeeberestclient/zeebe/FakeZeebeClientLifecycle.kt
@@ -0,0 +1,19 @@
+package com.github.korthout.zeeberestclient.zeebe
+
+import io.camunda.zeebe.client.api.response.Topology
+import io.camunda.zeebe.spring.client.annotation.processor.ZeebeAnnotationProcessorRegistry
+import io.camunda.zeebe.spring.client.lifecycle.ZeebeClientLifecycle
+import io.camunda.zeebe.spring.client.lifecycle.ZeebeClientObjectFactory
+import org.springframework.context.ApplicationEventPublisher
+
+/** Fake to replace the ZeebeClient with a FakeZeebeClient in the autowired ZeebeClientLifecycle */
+class FakeZeebeClientLifecycle :
+  ZeebeClientLifecycle(
+    ZeebeClientObjectFactory { FakeZeebeClient },
+    ZeebeAnnotationProcessorRegistry(java.util.ArrayList()),
+    ApplicationEventPublisher {}) {
+
+  fun onTopologyRequest(topology: Topology) {
+    FakeZeebeClient.topology = topology
+  }
+}
