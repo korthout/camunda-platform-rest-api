d8dd80169fed907e3782f789ea6e600803c74d64
Nikola Koevski <nikola.koevski@camunda.com>
2022-12-16 14:40:58 +0100

    fix(job): handle possible NPE for retryBackOff

--- src/main/kotlin/com/github/korthout/zeeberestclient/JobController.kt
+++ src/main/kotlin/com/github/korthout/zeeberestclient/JobController.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.Duration
 
 @RestController
 @RequestMapping("/jobs")
@@ -111,7 +112,7 @@ class JobController {
       client
         .newFailCommand(key)
         .retries(request.retries)
-        .retryBackoff(request.retryBackOff?.toDuration())
+        .retryBackoff(request.retryBackOff?.toDuration() ?: Duration.ZERO)
         .errorMessage(request.errorMessage ?: "")
         .send()
         .thenApply { ResponseEntity.noContent().build<Response<Nothing>>() }
