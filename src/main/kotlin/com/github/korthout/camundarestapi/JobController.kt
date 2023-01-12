b7cfe6ea4a84a0fa07a81c77cb51ce5d0f8372fe
Nico Korthout <nico.korthout@camunda.com>
2023-01-12 21:19:17 +0100

    refactor: add openapi spec PATCH /jobs
    This leaves the behavior exactly the same as before, but adds the
    openapi spec for the PATCH /jobs controller.
    
    The specification produces the exact same behavior as before, with one
    exception:
    - the error message when `status` field is null or undefined is
      different. It is now directly provided by SpringBoot (via Jackson).
      This produces an rather ugly message, because it leaks implementation
      details like specific classes involved such.
    
    Even though there is a change in the behavior, I'm still gonna call this
    a refactor 🤷

--- src/main/kotlin/com/github/korthout/camundarestapi/JobController.kt
+++ src/main/kotlin/com/github/korthout/camundarestapi/JobController.kt
@@ -1,12 +1,8 @@
 package com.github.korthout.camundarestapi
 
 import com.blueanvil.toDuration
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.korthout.camundarestapi.apis.JobsApi
-import com.github.korthout.camundarestapi.models.ActivatedJobsResponse
-import com.github.korthout.camundarestapi.models.ActivatedJobsResponseData
-import com.github.korthout.camundarestapi.models.Job
+import com.github.korthout.camundarestapi.models.*
 import io.camunda.zeebe.spring.client.lifecycle.ZeebeClientLifecycle
 import java.time.Duration
 import org.springframework.beans.factory.annotation.Autowired
@@ -72,19 +68,15 @@ class JobController : JobsApi {
           .join()
     }
 
-  /** Update a job. */
-  @PatchMapping("/jobs/{key}")
-  fun updateJob(
-    @PathVariable("key") key: Long,
-    @RequestBody body: UpdateJobRequest
-  ): ResponseEntity<Response<Nothing>> =
+  override fun updateJob(key: Long, body: UpdateJobRequest): ResponseEntity<UpdateJobResponse> =
     when {
       !client.isRunning ->
         ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
           .body(
-            Response(
-              "Unable to connect to Zeebe cluster." +
-                " Please try again, or check the configuration settings."))
+            UpdateJobResponse(
+              error =
+                "Unable to connect to Zeebe cluster." +
+                  " Please try again, or check the configuration settings."))
       else ->
         when (body.status) {
           "completed" ->
@@ -92,8 +84,10 @@ class JobController : JobsApi {
               .newCompleteCommand(key)
               .variables(body.variables ?: emptyMap())
               .send()
-              .thenApply { ResponseEntity.noContent().build<Response<Nothing>>() }
-              .exceptionally { ResponseEntity.badRequest().body(Response(it.cause.toString())) }
+              .thenApply { ResponseEntity.noContent().build<UpdateJobResponse>() }
+              .exceptionally {
+                ResponseEntity.badRequest().body(UpdateJobResponse(error = it.cause.toString()))
+              }
               .toCompletableFuture()
               .join()
           "failed" -> processFailJobRequest(key, body)
@@ -101,16 +95,17 @@ class JobController : JobsApi {
           else ->
             ResponseEntity.badRequest()
               .body(
-                Response(
-                  "Expected body property `status` to be one of `[completed]`," +
-                    " but it's `${body.status}`."))
+                UpdateJobResponse(
+                  error =
+                    "Expected body property `status` to be one of `[completed]`," +
+                      " but it's `${body.status}`."))
         }
     }
 
   fun processFailJobRequest(
     key: Long,
     request: UpdateJobRequest
-  ): ResponseEntity<Response<Nothing>> =
+  ): ResponseEntity<UpdateJobResponse> =
     if (request.retries != null) {
       client
         .newFailCommand(key)
@@ -118,45 +113,40 @@ class JobController : JobsApi {
         .retryBackoff(request.retryBackOff?.toDuration() ?: Duration.ZERO)
         .errorMessage(request.errorMessage ?: "")
         .send()
-        .thenApply { ResponseEntity.noContent().build<Response<Nothing>>() }
-        .exceptionally { ResponseEntity.badRequest().body(Response(it.cause.toString())) }
+        .thenApply { ResponseEntity.noContent().build<UpdateJobResponse>() }
+        .exceptionally {
+          ResponseEntity.badRequest().body(UpdateJobResponse(error = it.cause.toString()))
+        }
         .toCompletableFuture()
         .join()
     } else {
       ResponseEntity.badRequest()
         .body(
-          Response("Expected body property `retries` to be provided, but it's null or undefined."))
+          UpdateJobResponse(
+            error = "Expected body property `retries` to be provided, but it's null or undefined."))
     }
 
   fun processThrowErrorRequest(
     key: Long,
     request: UpdateJobRequest
-  ): ResponseEntity<Response<Nothing>> =
+  ): ResponseEntity<UpdateJobResponse> =
     if (request.errorCode != null) {
       client
         .newThrowErrorCommand(key)
         .errorCode(request.errorCode)
         .errorMessage(request.errorMessage ?: "")
         .send()
-        .thenApply { ResponseEntity.noContent().build<Response<Nothing>>() }
-        .exceptionally { ResponseEntity.badRequest().body(Response(it.cause.toString())) }
+        .thenApply { ResponseEntity.noContent().build<UpdateJobResponse>() }
+        .exceptionally {
+          ResponseEntity.badRequest().body(UpdateJobResponse(error = it.cause.toString()))
+        }
         .toCompletableFuture()
         .join()
     } else {
       ResponseEntity.badRequest()
         .body(
-          Response(
-            "Expected body property `errorCode` to be provided, but it's null or undefined."))
+          UpdateJobResponse(
+            error =
+              "Expected body property `errorCode` to be provided, but it's null or undefined."))
     }
-
-  data class UpdateJobRequest
-  @JsonCreator
-  constructor(
-    @JsonProperty("status", required = true) val status: String,
-    @JsonProperty("retries", required = false) val retries: Int?,
-    @JsonProperty("retryBackOff", required = false) val retryBackOff: String?,
-    @JsonProperty("errorCode", required = false) val errorCode: String?,
-    @JsonProperty("errorMessage", required = false) val errorMessage: String?,
-    @JsonProperty("variables", defaultValue = "{}") val variables: Map<String, Any>?
-  )
 }
