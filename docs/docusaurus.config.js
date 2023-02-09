111be0496398385d4494a4b22254557b6f2229cc
Nico Korthout <nico.korthout@camunda.com>
2023-02-09 16:05:56 +0100

    docs: support the edit this page button

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -28,8 +28,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          //editUrl:
-          //  "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/korthout/camunda-platform-rest-api/tree/main/docs",
           docLayoutComponent: "@theme/DocPage",
           docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
         },
