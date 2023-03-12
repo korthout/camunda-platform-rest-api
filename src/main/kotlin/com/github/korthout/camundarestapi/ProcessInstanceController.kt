99e45424ec4af38a71b2eee489af4a1db85c3e5c
Nico Korthout <nicokorthout@gmail.com>
2023-03-12 17:52:16 +0100

    feat: add GET /process-instances/{key} operation
    Adds the implementation for this operation. It uses Operate to retrieve
    the data. This requires some configuration to connect to Operate.
    
    Instead of writing my own configuration to connect to Operate, I copied
    the implementation used by the connectors. It is likely that this
    implementation will eventually move to spring-zeebe, at which point we
    can remove the copied java classes. For now, these should provide
    configurations to connect to Operate in SaaS, Self-Managed with
    Keycloak, and Self-Managed using a simple username/password. The details
    should be documented in the getting started / configuration.
    
    Since the camunda-operate-client-java only provides java.util.time.Date
    objects for the start and end datetime of the process instance, we need
    to assume the timezone. We maybe able to deal with this in a more
    sufisticated manner but this simple approach would provide value in most
    common cases already.

--- src/main/kotlin/com/github/korthout/camundarestapi/ProcessInstanceController.kt
+++ src/main/kotlin/com/github/korthout/camundarestapi/ProcessInstanceController.kt
@@ -1,11 +1,16 @@
 package com.github.korthout.camundarestapi
 
 import com.github.korthout.camundarestapi.apis.ProcessInstancesApi
-import com.github.korthout.camundarestapi.models.CreateProcessInstanceRequest
-import com.github.korthout.camundarestapi.models.ProcessInstance
-import com.github.korthout.camundarestapi.models.ProcessInstanceResponse
+import com.github.korthout.camundarestapi.models.*
+import com.github.korthout.camundarestapi.models.ProcessInstanceDetails.Status
+import com.github.korthout.camundarestapi.operate.OperateClientLifecycle
+import io.camunda.operate.dto.ProcessInstanceState
+import io.camunda.operate.exception.OperateException
 import io.camunda.zeebe.client.api.command.CreateProcessInstanceCommandStep1.CreateProcessInstanceCommandStep3
 import io.camunda.zeebe.spring.client.lifecycle.ZeebeClientLifecycle
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*
 class ProcessInstanceController : ProcessInstancesApi {
 
   @Autowired lateinit var client: ZeebeClientLifecycle
+  @Autowired lateinit var operateClient: OperateClientLifecycle
 
   override fun createProcessInstance(
     body: CreateProcessInstanceRequest
@@ -54,6 +60,47 @@ class ProcessInstanceController : ProcessInstancesApi {
                 "Expected body to contain either `bpmnProcessId` or `processDefinitionKey`, but both are null."))
     }
 
+  override fun getProcessInstance(key: Long): ResponseEntity<ProcessInstanceDetailsResponse> {
+    if (!operateClient.isRunning) {
+      return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+        .body(
+          ProcessInstanceDetailsResponse(
+            error =
+              "Unable to connect to Operate. " +
+                "Please try again, or check the configuration settings."))
+    }
+    val processInstance =
+      try {
+        operateClient.getProcessInstance(key) ?: return ResponseEntity.notFound().build()
+      } catch (e: OperateException) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+          .body(
+            ProcessInstanceDetailsResponse(
+              error =
+                "Unable to retrieve Process Instance '${key}' from Operate. ${e.message} " +
+                  "Operate may lag behind the workflow engine (Zeebe). " +
+                  "Please try again, or check the configuration settings."))
+      }
+    return ResponseEntity.ok(
+      ProcessInstanceDetailsResponse(
+        ProcessInstanceDetails(
+          processDefinitionKey = processInstance.processDefinitionKey,
+          bpmnProcessId = processInstance.bpmnProcessId,
+          version = processInstance.processVersion?.toInt(),
+          processInstanceKey = processInstance.key,
+          parentInstanceKey = processInstance.parentKey,
+          status =
+            when (processInstance.state) {
+              ProcessInstanceState.ACTIVE -> Status.activated
+              ProcessInstanceState.INCIDENT -> Status.hasIncident
+              ProcessInstanceState.COMPLETED -> Status.completed
+              ProcessInstanceState.CANCELED -> Status.terminated
+              null -> null
+            },
+          startedAt = toIso8601(processInstance.startDate),
+          endedAt = toIso8601(processInstance.endDate))))
+  }
+
   fun send(command: CreateProcessInstanceCommandStep3): ResponseEntity<ProcessInstanceResponse> =
     command
       .send()
@@ -68,4 +115,12 @@ class ProcessInstanceController : ProcessInstancesApi {
       }
       .toCompletableFuture()
       .join()
+
+  companion object {
+    private fun toIso8601(date: Date?) =
+      when (date) {
+        null -> null
+        else -> LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toString()
+      }
+  }
 }
